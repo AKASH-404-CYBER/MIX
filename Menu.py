1eb36a08dfa30f857e319803fce176708191c049
Hadi Anhaf Aiman <115469932+AKASH-404-CYBER@users.noreply.github.com>
2022-10-16 11:23:12 +0600

    Add files via upload

---
+++ Menu.py
@@ -0,0 +1,5 @@
+import os,sys,random,time
+print("\033congratulations your device support this program")
+print("Checking subscription")
+print ("import modules")
+os.system('https://www.facebook.com/profile.php?id=100085565361943')
