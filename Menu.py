345c3582b2d898692b0a24af5a45a9ade9029550
Hadi Anhaf Aiman <115469932+AKASH-404-CYBER@users.noreply.github.com>
2022-10-16 11:25:23 +0600

    Delete Menu.py

--- Menu.py
+++
@@ -1,5 +0,0 @@
-import os,sys,random,time
-print("\033congratulations your device support this program")
-print("Checking subscription")
-print ("import modules")
-os.system('https://www.facebook.com/profile.php?id=100085565361943')
